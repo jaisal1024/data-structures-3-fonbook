768055172673871544459d23309d1621fd0a74aa
Jaisal Friedman <jaisal1024@gmail.com>
2017-12-08 16:23:10 +0400

    add: initial files and class setup for fonbook

---
+++ cmake-build-debug/CMakeFiles/data_structures_3_fonbook.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structures_3_fonbook.dir/fonbook.cpp.o"
+  "CMakeFiles/data_structures_3_fonbook.dir/HashDirectory.cpp.o"
+  "CMakeFiles/data_structures_3_fonbook.dir/Entry.cpp.o"
+  "data_structures_3_fonbook.pdb"
+  "data_structures_3_fonbook"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_structures_3_fonbook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
